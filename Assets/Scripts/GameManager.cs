78d9dc91348f9b2c6219f47f190f91842c843dd7
Tharaka W.C.M.K <it18001112@my.sliit.lk>
2021-06-28 17:57:35 +0530

    Almost Finalized

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,11 @@ namespace Com.Cdap.CLSubComponent
     public class GameManager : MonoBehaviourPunCallbacks
     {
 
+        #region Public Fields
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+        #endregion
+
         #region Photon Fields
         public static GameManager Instance;
 
@@ -21,6 +26,24 @@ namespace Com.Cdap.CLSubComponent
         void Start()
         {
             Instance = this;
+
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else
+            {
+                if (PlayerManagerr.LocalPlayerInstance == null)
+                {
+                    Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                }
+                else
+                {
+                    Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+                }
+            }
         }
 
         #endregion
--- Assets/Scripts/PlayerManagerr.cs
+++ Assets/Scripts/PlayerManagerr.cs
@@ -18,12 +18,24 @@ namespace Com.Cdap.CLSubComponent
         private GameObject beams;
         //True, when the user is firing
         bool IsFiring;
+
+        #if UNITY_5_4_OR_NEWER
+        void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+        {
+            this.CalledOnLevelWasLoaded(scene.buildIndex);
+        }
+        #endif
+
         #endregion
 
         #region Public Fields
 
         [Tooltip("The current Health of our player")]
         public float Health = 1f;
+
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
         #endregion
 
         #region IPunObservable implementation
@@ -62,6 +74,14 @@ namespace Com.Cdap.CLSubComponent
             {
                 beams.SetActive(false);
             }
+
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                PlayerManagerr.LocalPlayerInstance = this.gameObject;
+            }
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
         }
 
         /// <summary>
@@ -83,6 +103,11 @@ namespace Com.Cdap.CLSubComponent
             {
                 Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
             }
+
+            #if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+            #endif
         }
 
         /// <summary>
@@ -148,6 +173,33 @@ namespace Com.Cdap.CLSubComponent
             // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
             Health -= 0.1f * Time.deltaTime;
         }
+
+        #if !UNITY_5_4_OR_NEWER
+        /// <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+        void OnLevelWasLoaded(int level)
+        {
+            this.CalledOnLevelWasLoaded(level);
+        }
+        #endif
+
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+        }
+
+        #if UNITY_5_4_OR_NEWER
+        public override void OnDisable()
+        {
+            // Always call the base to remove callbacks
+            base.OnDisable ();
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+        #endif
 
         #endregion
 
